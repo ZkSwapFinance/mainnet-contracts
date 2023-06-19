768cdd3758996aeedd9e93cc00f1c60f2a1e18f9
zkswap.finance <zkswap.finance@gmail.com>
2023-06-19 11:56:46 +0700

    Revert "init sources"
    This reverts commit 5c895b71f5ca16b1603bb6aa2f1aa13bf228dfbc.

--- contracts/interfaces/ERC20/IERC20WithMetadata.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-pragma solidity ^0.8.0;
-
-import './IERC20.sol';
-import './IERC20Metadata.sol';
-
-interface IERC20WithMetadata is IERC20, IERC20Metadata {}
--- contracts/interfaces/protocol/core/uniswap/IUniswapV2Callee.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-pragma solidity >=0.5.0;
-
-/// @dev Uniswap V2 callee interface
-interface IUniswapV2Callee {
-    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external;
-}
